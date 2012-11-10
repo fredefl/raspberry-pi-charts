a9f47b461bcda26e6d8ebfd3fcbfb659550dd676
fredefl <frederiklassen@gmail.com>
2012-11-10 15:43:39 +0100

    Added viewport.

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Admin Panel</title>
+		<!-- title -->
+		<title>Raspberry Pi Charts</title>
+		<!-- charset -->
+		<meta charset="utf-8">
+		<!-- viewport -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
 		<style type="text/css">
 			#page {
+				
 				width:auto;
 				margin-left:auto;
 				margin-right:auto;
