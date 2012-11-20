dad91833d3330da14841005afaf4cdac4d6edc5d
fredefl <frederiklassen@gmail.com>
2012-11-20 21:37:44 +0100

    Panic commit.

--- api.php
+++ api.php
@@ -1,3 +1,3 @@
 <?php
-echo '{"cpu":"' . trim(`ps aux|awk 'NR > 0 { s +=$3 }; END {print s}'`) . '","temp":"' . preg_replace('/[^0-9.]/', '', `/opt/vc/bin/vcgencmd measure_temp`) . '"}';
+echo '{"cpu":"' . trim(`ps aux|awk 'NR > 0 { s +=$3 }; END {print s}'`) . '","temperature":"' . preg_replace('/[^0-9.]/', '', `/opt/vc/bin/vcgencmd measure_temp`) . '"}';
 ?>
